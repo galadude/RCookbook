6df4abbb3a7dab9466e65376daad65be0f3600d1
Michael Galanakis <michael@galanakis.dk>
2014-09-12 08:01:08 +0200

    starting my cookbook

---
+++ category_table.R
@@ -0,0 +1,4 @@
+table <- array(data = c(509, 398, 116, 104),
+               dim = c(2, 2),
+               dimnames = list(c("Females", "Males"),
+                               c("Yes","No or undecided")))
---
+++ datalines.R
@@ -0,0 +1,6 @@
+toread <- "id name
+1 John
+2 Hanna
+3 George"
+
+survey <- read.table(textConnection(toread),header=T)
