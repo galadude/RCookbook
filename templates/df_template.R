79ab79edff72190a60a93ed0b72c3955c59a674a
Michael Galanakis <michael@galanakis.dk>
2014-09-12 08:44:02 +0200

    starting making templates

---
+++ templates/df_template.R
@@ -0,0 +1,3 @@
+# data.frame template
+df <- read.table("path", sep=",", header=T, encoding="UTF-8")
+df <- data.table(df)
---
+++ templates/knitr_template/data.R
@@ -0,0 +1,5 @@
+table <- array(data = c(509, 398, 116, 104),
+               dim = c(2, 2),
+               dimnames = list(c("Females", "Males"),
+                               c("Yes","No or undecided")))
+
